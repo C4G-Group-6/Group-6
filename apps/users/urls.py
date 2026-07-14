ef883250a5f556491053deeec39de22bc156f5b7
Ishaan <ishaans.desh@gmail.com>
2026-07-14 12:35:01 -0600

    added the remove from favorites button to the proile page and also added the logout

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView  # <-- Added this import
 
 from . import views
 
@@ -8,7 +9,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
-    # --- Add this line here ---
+    path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),  # <-- Added this line
     path('favorites/add/', views.add_to_favorites, name='add_to_favorites'),
     path('favorites/remove/', views.remove_from_favorites, name='remove_from_favorites'),
 ]
